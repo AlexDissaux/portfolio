53f52344e6a5f2ed88fbba9749cf56236110909d
AlexDissaux <dissauxalex@gmail.com>
2020-10-19 22:59:40 +0200

    Mise en place archotecture header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
+import Header from './Header/Header.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Header/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+/*
+function tick() {
+    const element = (
+        <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    // highlight-next-line
+    ReactDOM.render(element, document.getElementById('root'));
+}*/
+
+//setInterval(tick, 1000);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
