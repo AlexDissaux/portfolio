12df61bc3f8e95cf47628f5f19a2d06748ee9c0b
Dissaux Alexandre <31765537+AlexDissaux@users.noreply.github.com>
2020-10-22 11:54:07 +0200

    Changing the photo's HTML Tag to img

--- src/Header/Header2.js
+++ src/Header/Header2.js
@@ -8,7 +8,7 @@ import '../index.css'
 function Header(props) {
     return (
         <div class="flex-container">
-            <div id="photo" class="item animated"> Photo </div>
+            <img id="photo" class="item animated" src="https://mdn.mozillademos.org/files/7693/catfront.png" alt="">
             <div id="title" class="item"> Title </div>
         </div>
     );
