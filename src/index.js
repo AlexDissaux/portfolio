cb9dd35da098a69b3ce8add728b7cd2de30fba36
AlexDissaux <dissauxalex@gmail.com>
2020-10-23 17:33:58 +0200

    bug fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css'
 import Header from './Header/Header2.js';
 import SpaceTop from './spaceTop'
 import Tech from "./Tech/Tech";
