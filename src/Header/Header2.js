d68efaaae7dfe029a98dffc7baa633adefec5b46
AlexDissaux <dissauxalex@gmail.com>
2020-10-23 17:40:51 +0200

    maj

--- src/Header/Header2.js
+++ src/Header/Header2.js
@@ -9,10 +9,14 @@ function Header(props) {
     return (
         <div class="flex-container">
             <img id="photo" class="item animated" src="https://media-exp1.licdn.com/dms/image/C4E03AQG0YHLh345PxQ/profile-displayphoto-shrink_200_200/0?e=1608768000&v=beta&t=6xfc4EKVSfWRFcdPLWr8XFi3anItc-TRNxNdqfcM59A" alt=""/>
-            <div id="title" class="item text"> Title </div>
+            <div id="title" className="item text">
+                <h1>Ceci est un titre de qualité inégalé</h1>
+                <h2>Ceci est un sous-titre des plus improbables et incroyable adazdbzkjn zeofh he zof lorem impsum</h2>
+            </div>
         </div>
     );
 }
+
 
 
 
